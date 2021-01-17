050d9246d71beafb8fc7e449b44f9e2525e8097b
Frank <frankwoord@live.nl>
2021-01-17 21:46:34 +0100

    Fix: Responsiveness on tabloids

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './App.css';
 
-function fetchWeather(lat = 52.7138816, lon = 5.8621951999999995, key){
+function fetchWeather(lat = 52.7138816, lon = 5.8621951999999995, key) {
   const url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appid=${key}`;
-  return  fetch(url)
+  return fetch(url)
     .then((res) => res.json())
- }
+}
 
- function importAll(r) {
+function importAll(r) {
   let images = {};
   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
   return images;
@@ -42,69 +42,88 @@ function App() {
       })
   }, [coordinates]);
 
-  function handleLocation(pos){
+  function handleLocation(pos) {
     setCoordinates({
       latitude: pos.coords.latitude,
       longitude: pos.coords.longitude,
     })
   }
 
-  if(loading) return <p>Loading</p>;
+  if (loading) return <p>Loading</p>;
 
-  if(error){
+  if (error) {
     return (
       <>
-      <p>{error}</p>
-      <button>try again</button>
+        <p>{error}</p>
+        <button>try again</button>
       </>
     )
   }
 
-  console.log(weather.daily);
 
+  function weekInfo(dayNumber){ 
+    return( 
+      <li className='tab'>
+        <b> {new Date(weather.daily[dayNumber].dt * 1000).toLocaleString("en-US", { weekday: "long" })}</b>: 
+          <p style={{marginLeft: '.5rem'}}>{Math.round(weather.daily[dayNumber].temp.min)}°C 
+        - {Math.round(weather.daily[dayNumber].temp.max)}°C </p>
+      </li>
+    )
+  }
+
+  function getBackground(day) {
+    return images[weather.daily[day].weather[0].main + '.jpg'].default;
+  }
+
+  function getCurrentBackground() {
+    var url = images[weather.current.weather[0].main + '.jpg'].default;
+    return url;
+  }
   return (
+
     !weather ? <p>Loading</p> :
-    <div className="App">
-      <div className="today">
-         <ul>
-           <h1>Today</h1>
-           <li>{weather.current.weather[0].description}</li>
-           <li>{Math.round(weather.current.temp)}°C</li>
-           <li>Feels like : {Math.round(weather.current.feels_like)}°C</li>
-        </ul>
+
+      <div className="App">
+        <div className="today" style={{ backgroundImage: `url(${getCurrentBackground()})` }}>
+          <ul>
+            <h1>Today</h1>
+            <li>{weather.current.weather[0].description}</li>
+            <li>{Math.round(weather.current.temp)}°C</li>
+            <li>Feels like : {Math.round(weather.current.feels_like)}°C</li>
+          </ul>
+        </div>
+        <div className="tom" style={{ backgroundImage: `url(${getBackground(0)})` }}>
+          <ul>
+            <h1>Tomorrow</h1>
+            <li>{weather.daily[0].weather[0].description}</li>
+            <li>{Math.round(weather.daily[0].temp.day)}°C</li>
+            <li>Feels like : {Math.round(weather.daily[0].feels_like.day)}°C</li>
+          </ul>
+        </div>
+        <div className="DaT" style={{ backgroundImage: `url(${getBackground(1)})` }}>
+          <ul>
+            <h1>Day after Tomorrow</h1>
+            <li>{weather.daily[1].weather[0].description}</li>
+            <li>{Math.round(weather.daily[1].temp.day)}°C</li>
+            <li>Feels like : {Math.round(weather.daily[1].feels_like.day)}°C</li>
+          </ul>
+        </div>
+        <div className="week">
+          <ul>
+            <h1>This week!</h1>
+            {weekInfo(0)}
+            {weekInfo(1)}
+            {weekInfo(2)}
+            {weekInfo(3)}
+            {weekInfo(4)}
+            {weekInfo(5)}
+            {weekInfo(6)}
+          </ul>
+        </div>
+        <div className="share">
+          <h1>Weather at {weather.lat}, {weather.lon}</h1>
+        </div>
       </div>
-      <div className="tom">
-        <ul>
-           <h1>Tomorrow</h1>
-           <li>{weather.daily[0].weather[0].description}</li>
-           <li>{Math.round(weather.daily[0].temp.day)}°C</li>
-           <li>Feels like : {Math.round(weather.daily[0].feels_like.day)}°C</li>
-        </ul>
-      </div>
-      <div className="DaT">
-        <ul>
-           <h1>Day after Tomorrow</h1>
-           <li>{weather.daily[1].weather[0].description}</li>
-           <li>{Math.round(weather.daily[1].temp.day)}°C</li>
-           <li>Feels like : {Math.round(weather.daily[1].feels_like.day)}°C</li>
-        </ul>
-      </div>
-      <div className="week">
-        <ul>  
-          <h1>This week!</h1>
-           <li>{new Date(weather.daily[0].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[0].temp.min)}°C and {Math.round(weather.daily[0].temp.max)}°C</li>
-           <li>{new Date(weather.daily[1].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[1].temp.min)}°C and  {Math.round(weather.daily[1].temp.max)}°C</li>
-           <li>{new Date(weather.daily[2].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[2].temp.min)}°C and  {Math.round(weather.daily[2].temp.max)}°C</li>
-           <li>{new Date(weather.daily[3].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[3].temp.min)}°C and  {Math.round(weather.daily[3].temp.max)}°C</li>
-           <li>{new Date(weather.daily[4].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[4].temp.min)}°C and  {Math.round(weather.daily[4].temp.max)}°C</li>
-           <li>{new Date(weather.daily[5].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[5].temp.min)}°C and  {Math.round(weather.daily[5].temp.max)}°C</li>
-           <li>{new Date(weather.daily[6].dt *1000).toLocaleString("en-US", {weekday: "long"})}: Between {Math.round(weather.daily[5].temp.min)}°C and  {Math.round(weather.daily[6].temp.max)}°C</li>
-        </ul>
-      </div>
-      <div className="share">
-        <h1>Weather at {weather.lat}, {weather.lon}</h1>
-        </div> 
-    </div>
   );
 }
 
